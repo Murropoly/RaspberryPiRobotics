053e5fce395692ea2070c1193d50dcd6564731f8
Murropoly <tylermuranaka@gmail.com>
2019-01-30 12:24:34 -0700

    Create RPi_Rover.py

---
+++ RPi_Rover.py
@@ -0,0 +1,54 @@
+#Imports all needed packages
+import time
+import RPi.GPIO as GPIO
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+#GPIO pin setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(a, GPIO.OUT)
+GPIO.setup(b, GPIO.OUT)
+GPIO.setup(c, GPIO.OUT)
+GPIO.setup(d, GPIO.OUT)
+
+#Makes the robot go forward
+def Forward():
+    GPIO.output(a,False)
+    GPIO.output(b,True)
+    GPIO.output(c,False)
+    GPIO.output(d,True)
+
+#Makes the robot go in REVERSE
+def Reverse():
+    GPIO.output(a,True)
+    GPIO.output(b,False)
+    GPIO.output(c,True)
+    GPIO.output(d,False)
+
+#Turns the robot RIGHT
+def TurnRight():
+    GPIO.output(a,True)
+    GPIO.output(b,False)
+    GPIO.output(c,False)
+    GPIO.output(d,True)
+
+#Turns the robot LEFT
+def TurnLeft():
+    GPIO.output(a,False)
+    GPIO.output(b,True)
+    GPIO.output(c,True)
+    GPIO.output(d,False)
+
+#STOPS the Robots motors completly
+def Stop():
+    GPIO.output(a,False)
+    GPIO.output(b,False)
+    GPIO.output(c,False)
+    GPIO.output(d,False)
+
+def Cleanup():
+    print("Cleaning Up GPIO Pins")
+    GPIO.cleanup()
