306dd3f89186b47ca41ac76e66f984e25a7ced1e
Murropoly <tylermuranaka@gmail.com>
2019-01-30 21:22:27 -0700

    controller code

--- ControllerCodeTemplates/BlueDotControl.py
+++ ControllerCodeTemplates/BlueDotControl.py
@@ -1,24 +1,17 @@
-import RPiRobot
 from bluedot import BlueDot
 from signal import pause
 import RPi.GPIO as GPIO
 import time
 
-Forward() = RPiRobot.Forward()
-Reverse() = RPiRobot.Reverse()
-TurnRight() = RPiRobot.TurnRight()
-TurnLeft() = RPiRobot.TurnLeft()
-Stop() = RPiRobot.Stop()
-
 def dpad(pos):
     if pos.top:
-        Forward()
+        
     elif pos.bottom:
-        Reverse()
+        
     elif pos.left:
-        TurnRight()
+        
     elif pos.right:
-        TurnLeft()
+        
     elif pos.middle:
         
 bd = BlueDot()
